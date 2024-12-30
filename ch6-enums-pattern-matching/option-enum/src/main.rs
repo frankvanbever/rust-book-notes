c914384bef0871574ac48a388cc1769c3eca54e4
Frank Vanbever <frank.vanbever@gmail.com>
2024-12-30 17:40:29 +0100

    Finished option Enum part
    Signed-off-by: Frank Vanbever <frank.vanbever@gmail.com>

---
+++ ch6-enums-pattern-matching/option-enum/src/main.rs
@@ -0,0 +1,13 @@
+
+fn main() {
+    println!("Hello, world!");
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+    let sum = x + y;
+}
