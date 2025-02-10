31a4e6bd322aa3dee7eb4c1a65679aa332db5a6b
Frank Vanbever <frank.vanbever@gmail.com>
2025-02-10 20:59:42 +0100

    At ch7.3 Module Tree Paths
    Finished "Starting Relative Paths with `super`"

--- ch7-packages-modules-crates/restaurant/src/lib.rs
+++ ch7-packages-modules-crates/restaurant/src/lib.rs
@@ -1,6 +1,7 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        // not only the module but the function needs to be public!
         fn seat_at_table() {}
     }
 
@@ -9,4 +10,24 @@ mod front_of_house {
         fn serve_order() {}
         fn take_payment() {}
     }
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+       cook_order();
+        // super is like ".." in a filesystem path
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
