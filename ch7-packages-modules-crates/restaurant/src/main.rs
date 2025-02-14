835820083cfbab34dd63e71feaafc201e628413f
Frank Vanbever <frank.vanbever@gmail.com>
2025-02-14 22:28:38 +0100

    Finished ch7.4

---
+++ ch7-packages-modules-crates/restaurant/src/main.rs
@@ -0,0 +1,16 @@
+use std::collections::*;
+//use std::fmt;
+//use std::io::Result as IoResult;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1,2);
+}
+
+//fn function1() -> fmt::Result {
+//    // --snip--
+//}
+
+//fn function2() -> IoResult<()> {
+//    // --snip--
+//}
