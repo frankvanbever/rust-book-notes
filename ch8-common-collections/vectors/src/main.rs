21d5fe079330de45f9f9b796339cfb7394b78755
Frank Vanbever <frank.vanbever@gmail.com>
2025-02-17 21:53:42 +0100

    Finished Reading Elements of Vectors

--- ch8-common-collections/vectors/src/main.rs
+++ ch8-common-collections/vectors/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
 
     //let v2 = vec![1, 2, 3];
 
-    let v = vec![1, 2, 3, 4, 5];
+    let mut v = vec![1, 2, 3, 4, 5];
     let third: &i32 = &v[2];
     println!("The third element is {third}");
 
@@ -18,4 +18,17 @@ fn main() {
         Some(third) => println!("The third element is {third}"),
         None => println!("There is no third element."),
     }
+
+    // This fails at runtime
+    //let _does_not_exist = &v[100];
+    //let _does_not_exist2 = v.get(100);
+
+
+    // This is not allowed because the push might cause the memory
+    // to reallocate. The push creates a mutable borrow after the
+    // immutable borrow to first. This is not allowed.
+    //let first = &v[0];
+    //v.push(6);
+
+    //println!("The first element is: {first}");
 }
