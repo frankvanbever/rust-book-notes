bb17c1c70138a16fb79841ca3540b2b73af9050d
Frank Vanbever <frank.vanbever@gmail.com>
2025-02-18 21:51:53 +0100

    Finished ch8 - vectors

--- ch8-common-collections/vectors/src/main.rs
+++ ch8-common-collections/vectors/src/main.rs
@@ -1,3 +1,4 @@
+
 fn main() {
     //println!("Hello, world!");
 
@@ -9,7 +10,7 @@ fn main() {
 
     //let v2 = vec![1, 2, 3];
 
-    let mut v = vec![1, 2, 3, 4, 5];
+    let v = vec![1, 2, 3, 4, 5];
     let third: &i32 = &v[2];
     println!("The third element is {third}");
 
@@ -31,4 +32,34 @@ fn main() {
     //v.push(6);
 
     //println!("The first element is: {first}");
+
+    // Iterating over the Values in a Vector
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+
+    for i in &v {
+        println!("{i}");
+    }
+
+    // Using an Enum to Store Multiple Types
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    // When a vector goes out of scope it is automatically dropped.
+    // This means that the contents of the vector also get dropped.
+    // The borrow checker ensures that any references to vector
+    // contents are only used when the vector itself is valid
 }
