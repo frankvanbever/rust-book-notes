54c42242ad02efcda5e20819e64af7321f00f1f6
Frank Vanbever <frank.vanbever@gmail.com>
2025-02-22 22:44:10 +0100

    ch8 - Finished hashmap
    Next up are the exercises

--- ch8-common-collections/hashmaps/src/main.rs
+++ ch8-common-collections/hashmaps/src/main.rs
@@ -12,4 +12,44 @@ fn main() {
     for (key, value) in &scores {
         println!("{key} scored {value}");
     }
+
+    // Ownership
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    //println!("{field_name}");
+    // Doing this is invalid because the values have been moved
+    // into the hashmap. This would not be an issue if we
+    // inserted references but that raises other problems with
+    // the validity of the lifetime.
+
+    // Updating
+    // Overwriting
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{scores:?}");
+
+    // Adding only if a key isn't present
+
+    scores.insert(String::from("Blue"), 10);
+    scores.entry(String::from("Green")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{scores:?}");
+
+    // Updating a value based on the old value
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{map:?}");
 }
