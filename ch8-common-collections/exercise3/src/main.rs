01b0c875af8397ab3c91018e4003d74550f44023
Frank Vanbever <frank.vanbever@gmail.com>
2025-03-05 22:14:57 +0100

    Started ch8 exercise 3

---
+++ ch8-common-collections/exercise3/src/main.rs
@@ -0,0 +1,15 @@
+use std::collections::HashMap;
+
+// Using a hash map and vectors, create a text interface that
+// allows a user to add employee names to a department in a company;
+// for example, "Ad Sally to Engineering" or "Add Amir to Sales".
+// Then let the user retrieve a list of all people in a department or
+// all people in the company by department sorted alphabetically.
+
+fn main() {
+    let mut departments = HashMap::new();
+
+    let engineering: Vec<String> = Vec::new();
+
+    departments.insert("Enginering", engineering);
+}
