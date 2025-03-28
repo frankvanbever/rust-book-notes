76473a2d8039d08bfe8b4a1280e5d7a0a2e0ee7c
Frank Vanbever <frank.vanbever@gmail.com>
2025-03-28 21:28:12 +0100

    Finished 9.2

--- ch9-error-handling/recoverable-errors/src/main.rs
+++ ch9-error-handling/recoverable-errors/src/main.rs
@@ -1,4 +1,25 @@
-use std::{fs::File, io::ErrorKind};
+use std::fs::File;
+use std::io::{self, Read, ErrorKind};
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    // let username_file_result = File::open("hello.txt");
+
+    // let mut username_file = match username_file_result {
+    //     Ok(file) => file,
+    //     Err(e) => return Err(e)
+    // };
+
+    // let mut username = String::new();
+
+    // match username_file.read_to_string(&mut username) {
+    //     Ok(_) => Ok(username),
+    //     Err(e) => Err(e),
+    // }
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
 
 fn main() {
     let greeting_file_result = File::open("hello.txt");
@@ -13,6 +34,11 @@ fn main() {
             other_error => {
                 panic!("Problem opening the file: {other_error:?}");
             }
-        },
+        }
     };
+
+    let _username = read_username_from_file();
+
+    // let greetings_file = File::open("hello.txt")
+    //     .expect("hello.txt should be included in this project");
 }
