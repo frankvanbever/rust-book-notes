f7d3fb2c398b250c91e56a030109108ad9c2dd4f
Frank Vanbever <frank.vanbever@gmail.com>
2025-04-30 22:05:31 +0200

    Finished listing 13-5

--- ch13-functional-features/closures/src/main.rs
+++ ch13-functional-features/closures/src/main.rs
@@ -59,4 +59,21 @@ fn main() {
 
     let result2 = add_one_v4(6);
     println!("{result2}");
+
+    let list = vec![1,2,3];
+    println!("Before defining closure: {list:?}");
+
+    let only_borrows = || println!("From closure: {list:?}");
+
+    println!("Before calling closure: {list:?}");
+    only_borrows();
+    println!("After calling closure: {list:?}");
+
+    let mut list2 = vec![1,2,3];
+    println!("Before defining closure: {list2:?}");
+
+    let mut borrows_mutably = || list2.push(7);
+
+    borrows_mutably();
+    println!("After calling closure: {list2:?}");
 }
