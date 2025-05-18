51f87cab1da0ea48d1ae863ecc92cbf7b412e3c3
Frank Vanbever <frank.vanbever@gmail.com>
2025-05-18 22:15:43 +0200

    Finished 13.3

--- ch12-io-project/minigrep/src/lib.rs
+++ ch12-io-project/minigrep/src/lib.rs
@@ -12,8 +12,6 @@ impl Config {
     pub fn build(mut args: impl Iterator<Item = String>,) -> Result<Config, &'static str> {
         args.next();
 
-
-
         let query = match args.next() {
             Some(arg) => arg,
             None => return Err("Didn't get a query string"),
@@ -52,15 +50,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results  = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
