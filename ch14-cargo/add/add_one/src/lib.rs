0dc585e8336694b4d7ab4283496264007fb6739c
Frank Vanbever <frank.vanbever@gmail.com>
2025-05-29 21:05:19 +0200

    Finished ch14, started ch15
    At "Using Box<T> to point to Data on the Heap"

--- ch14-cargo/add/add_one/src/lib.rs
+++ ch14-cargo/add/add_one/src/lib.rs
@@ -1,4 +1,3 @@
-use rand;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
@@ -14,7 +13,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(3,  add_one(2));
     }
 }
