53634488553f311482eb2f34fe2ada55b24085ea
Frank Vanbever <frank.vanbever@gmail.com>
2025-06-08 22:18:02 +0200

    At 15-5 Keeping Track of Borrows at Runtime

--- ch15-smart-pointers/interior-mutability/src/main.rs
+++ ch15-smart-pointers/interior-mutability/src/main.rs
@@ -1,30 +1,3 @@
-pub trait Messenger {
-    fn send(&self, msg: &str);
-}
-
-pub struct LimitTracker<'a, T: Messenger> {
-    messenger: &'a T,
-    value: usize,
-    max: usize,
-}
-
-impl <'a, T> LimitTracker<'a, T>
-    where
-    T: Messenger,
-{
-    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
-        LimitTracker { messenger,
-                       value: 0,
-                       max,
-        }
-    }
-
-    pub fn set_value(&mut self, value: usize) {
-        self.value = value;
-
-        let percentage of max = self.value as f64 / self.max as f64
-    }
-}
 
 fn main() {
     let x = 5;
