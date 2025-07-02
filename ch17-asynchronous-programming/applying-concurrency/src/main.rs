0dc5d049ac827e1c3e3e967cf180cc16ccd33cc3
Frank Vanbever <frank.vanbever@gmail.com>
2025-07-02 21:42:26 +0200

    listing 17-9

--- ch17-asynchronous-programming/applying-concurrency/src/main.rs
+++ ch17-asynchronous-programming/applying-concurrency/src/main.rs
@@ -1,22 +1,12 @@
-use std::time::Duration;
-
 
 fn main() {
     trpl::run(async {
-        let fut1 = async {
-            for i in 1..10 {
-                println!("hi number {i} from the first task!");
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        };
+        let (tx, mut rx) = trpl::channel();
 
-        let fut2 = async {
-            for i in 1..5 {
-                println!("hi number {i} from the second task!");
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        };
+        let val = String::from("hi");
+        tx.send(val).unwrap();
 
-        trpl::join(fut1, fut2).await;
+        let received = rx.recv().await.unwrap();
+        println!("Got: {received}");
     })
 }
