c1c1a62b4e510496f75e3d6df626e72a71090d47
Frank Vanbever <frank.vanbever@gmail.com>
2025-07-04 22:25:39 +0200

    Finished 17.3

--- ch17-asynchronous-programming/applying-concurrency/src/main.rs
+++ ch17-asynchronous-programming/applying-concurrency/src/main.rs
@@ -5,7 +5,9 @@ fn main() {
     trpl::run(async {
 
         let (tx, mut rx) = trpl::channel();
-        let tx_fut = async move {
+
+        let tx1 = tx.clone();
+        let tx1_fut = async move {
             let vals = vec![
                 String::from("hi"),
                 String::from("from"),
@@ -14,9 +16,24 @@ fn main() {
             ];
 
             for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
                 tx.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(500)).await;
             }
+
         };
 
         let rx_fut = async {
@@ -24,6 +41,7 @@ fn main() {
                 println!("received '{value}'");
             }
         };
-        trpl::join(tx_fut, rx_fut).await;
+
+        trpl::join3(tx1_fut, tx_fut, rx_fut).await;
     })
 }
