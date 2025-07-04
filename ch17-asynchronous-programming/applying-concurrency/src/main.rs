7147c9271a9c0bcc52c018df02676b9c1aff0e52
Frank Vanbever <frank.vanbever@gmail.com>
2025-07-04 22:17:42 +0200

    17.2 listing 17-12

--- ch17-asynchronous-programming/applying-concurrency/src/main.rs
+++ ch17-asynchronous-programming/applying-concurrency/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     trpl::run(async {
 
         let (tx, mut rx) = trpl::channel();
-        let tx_fut = async {
+        let tx_fut = async move {
             let vals = vec![
                 String::from("hi"),
                 String::from("from"),
