5ebf3fc57aea6e250b6148856df3528447f79b2b
Frank Vanbever <fvb@funkworks.be>
2025-12-01 09:06:24 +0100

    Chapter 20 - at listing 20-11

--- ch20-advanced-features/unsafe-rust/src/main.rs
+++ ch20-advanced-features/unsafe-rust/src/main.rs
@@ -1,7 +1,15 @@
+use std::slice;
 
 unsafe fn dangerous() {
     println!("Unsafe!")
 }
+
+unsafe extern "C" {
+    safe fn abs(input: i32) -> i32;
+}
+
+static HELLO_WORLD: &str = "Hello, world!";
+static mut COUNTER: u32 = 0;
 
 fn main() {
 
@@ -26,8 +34,52 @@ fn main() {
     let mut v = vec![1, 2, 3, 4, 5, 6];
     let r = &mut v[..];
 
-    let (a, b) = r.split_at_mut(3);
+    let (a, b) = split_at_mut(r, 3);
 
-    assert_eq!(a, &mut [1, 3, 3]);
+    assert_eq!(a, &mut [1, 2, 3]);
     assert_eq!(b, &mut [4, 5, 6]);
+
+    println!("Absolute value of -3 according to C: {}", abs(-3));
+
+    // Accessing or Modifygin a Mutable Static Variable
+    println!("value is: {HELLO_WORLD}");
+
+
+    unsafe {
+        // SAFETY: This is only called from a single thread in `main`.
+        add_to_count(3);
+        println!("COUNTER: {}", *(&raw const COUNTER));
+    }
+}
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    //(&mut values[..mid], &mut values[mid..])
+    // This fails because we're borrowing from the same slice twice. This is OK
+    // in this case because we're borrowing different parts of the slice but the
+    // compiler does not know this.
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+
+}
+
+
+#[unsafe(no_mangle)]
+pub extern "C" fn call_from_c() {
+    println!("Just called a Rust function from C!");
+}
+
+unsafe fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
 }
