2186fd9ff5fe6afe1e27a0a02bb5e50b54d3ac51
Frank Vanbever <fvb@funkworks.be>
2025-12-03 09:53:38 +0100

    At listing 20-20

--- ch20-advanced-features/advanced-traits/src/main.rs
+++ ch20-advanced-features/advanced-traits/src/main.rs
@@ -1,19 +1,112 @@
+use std::ops::Add;
 
-pub trait Iterator {
-    type Item; // This is a placeholder
+// pub trait Iterator {
+//     type Item; // This is a placeholder
 
-    fn next(&mut self) -> Option<Self::Item>;
+//     fn next(&mut self) -> Option<Self::Item>;
+// }
+
+// impl Iterator for Counter {
+//     type Item = u32;
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         // -- snip --
+//     }
+// }
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-impl Iterator for Counter {
-    type Item = u32;
+impl Add for Point {
+    type Output = Point;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        // -- snip --
+    fn add(self, rhs: Self) -> Self::Output {
+        Point {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
     }
 }
 
 
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+
+    let one_meter = Meters(1);
+    let five_millimeter = Millimeters(5);
+
+    let result = five_millimeter + one_meter;
+
+    println!("result is {}", result.0);
+
+    let person = Human;
+
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+    person.fly();
+
+    println!("A baby dog is called a {}", Dog::baby_name());
 }
