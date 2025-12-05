ae6eee804a38a47f8fafc3e8bf95d7c686355e8b
Frank Vanbever <fvb@funkworks.be>
2025-12-05 09:44:29 +0100

    Finished "Advanced Traits"

--- ch20-advanced-features/advanced-traits/src/main.rs
+++ ch20-advanced-features/advanced-traits/src/main.rs
@@ -110,6 +110,14 @@ impl fmt::Display for Point {
 
 impl OutlinePrint for Point {}
 
+// Using Newtype
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
 
 fn main() {
     assert_eq!(
@@ -143,4 +151,8 @@ fn main() {
 
     let printpoint = Point { x: 1, y: 2 };
     printpoint.outline_print();
+
+
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {w}");
 }
