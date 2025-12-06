287c44bd4e80215ff257a9814ff9890b5b29b45d
Frank Vanbever <fvb@funkworks.be>
2025-12-06 08:12:40 +0100

    Finished "ch20-03 Advanced Types"

--- ch20-advanced-features/advanced-types/src/main.rs
+++ ch20-advanced-features/advanced-types/src/main.rs
@@ -1,3 +1,33 @@
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+    // Creating Type Sysnonyms with Type Aliases
+    type Kilometers = i32;
+
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+
+    println!("x + y = {}", x + y);
+
+    // The main use case for Type Synonyms is to prevent lengthy types
+    let f: Box<dyn Fn() + Send + 'static> = Box::new(|| println!("hi"));
+
+    // fn takes_long_type(f: Box<dyn Fn() + Send + 'static>) {
+    //     // --snip
+    // }
+
+    // fn returns_long_type() -> Box<dyn Fn() + Send + 'static> {
+    //     let foo: Box<dyn Fn() + Send + 'static>;
+    // }
+
+    impl<T> Option<T> {
+        pub fn unwrap(self) -> T {
+            match self {
+                Some(val) => val
+
+            }
+        }
+    }
 }
